f89f84901ea2b7c9fa1e2b5901fabbd4c7a2a171
Mark Trevisiol <mark@trevisiol.com>
2026-02-16 14:14:44 -0800

    Add the real files to the repository

---
+++ WifiSocketTest/WifiSocketTest/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace WifiSocketTEst
+{
+   partial class Form1
+   {
+      /// <summary>
+      /// Required designer variable.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      /// Clean up any resources being used.
+      /// </summary>
+      /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+      protected override void Dispose(bool disposing)
+      {
+         if (disposing && (components != null))
+         {
+            components.Dispose();
+         }
+         base.Dispose(disposing);
+      }
+
+      #region Windows Form Designer generated code
+
+      /// <summary>
+      /// Required method for Designer support - do not modify
+      /// the contents of this method with the code editor.
+      /// </summary>
+      private void InitializeComponent()
+      {
+         this.components = new System.ComponentModel.Container();
+         this.SendaBtn = new System.Windows.Forms.Button();
+         this.LogLB = new System.Windows.Forms.ListBox();
+         this.SendbBtn = new System.Windows.Forms.Button();
+         this.SendcBtn = new System.Windows.Forms.Button();
+         this.SenddBtn = new System.Windows.Forms.Button();
+         this.timer1 = new System.Windows.Forms.Timer(this.components);
+         this.SuspendLayout();
+         // 
+         // SendaBtn
+         // 
+         this.SendaBtn.Location = new System.Drawing.Point(12, 12);
+         this.SendaBtn.Name = "SendaBtn";
+         this.SendaBtn.Size = new System.Drawing.Size(184, 44);
+         this.SendaBtn.TabIndex = 0;
+         this.SendaBtn.Text = "Send a";
+         this.SendaBtn.UseVisualStyleBackColor = true;
+         this.SendaBtn.MouseUp += new System.Windows.Forms.MouseEventHandler(this.SendaBtn_MouseUp);
+         // 
+         // LogLB
+         // 
+         this.LogLB.FormattingEnabled = true;
+         this.LogLB.Location = new System.Drawing.Point(12, 212);
+         this.LogLB.Name = "LogLB";
+         this.LogLB.Size = new System.Drawing.Size(184, 95);
+         this.LogLB.TabIndex = 1;
+         // 
+         // SendbBtn
+         // 
+         this.SendbBtn.Location = new System.Drawing.Point(12, 62);
+         this.SendbBtn.Name = "SendbBtn";
+         this.SendbBtn.Size = new System.Drawing.Size(184, 44);
+         this.SendbBtn.TabIndex = 0;
+         this.SendbBtn.Text = "Send b";
+         this.SendbBtn.UseVisualStyleBackColor = true;
+         this.SendbBtn.MouseUp += new System.Windows.Forms.MouseEventHandler(this.SendbBtn_MouseUp);
+         // 
+         // SendcBtn
+         // 
+         this.SendcBtn.Location = new System.Drawing.Point(12, 112);
+         this.SendcBtn.Name = "SendcBtn";
+         this.SendcBtn.Size = new System.Drawing.Size(184, 44);
+         this.SendcBtn.TabIndex = 0;
+         this.SendcBtn.Text = "Send c";
+         this.SendcBtn.UseVisualStyleBackColor = true;
+         this.SendcBtn.MouseUp += new System.Windows.Forms.MouseEventHandler(this.SendcBtn_MouseUp);
+         // 
+         // SenddBtn
+         // 
+         this.SenddBtn.Location = new System.Drawing.Point(12, 162);
+         this.SenddBtn.Name = "SenddBtn";
+         this.SenddBtn.Size = new System.Drawing.Size(184, 44);
+         this.SenddBtn.TabIndex = 0;
+         this.SenddBtn.Text = "Send d";
+         this.SenddBtn.UseVisualStyleBackColor = true;
+         this.SenddBtn.MouseUp += new System.Windows.Forms.MouseEventHandler(this.SenddBtn_MouseUp);
+         // 
+         // timer1
+         // 
+         this.timer1.Enabled = true;
+         this.timer1.Interval = 1000;
+         this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+         // 
+         // Form1
+         // 
+         this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         this.ClientSize = new System.Drawing.Size(237, 324);
+         this.Controls.Add(this.LogLB);
+         this.Controls.Add(this.SenddBtn);
+         this.Controls.Add(this.SendcBtn);
+         this.Controls.Add(this.SendbBtn);
+         this.Controls.Add(this.SendaBtn);
+         this.Name = "Form1";
+         this.Text = "Antenna Switch";
+         this.ResumeLayout(false);
+
+      }
+
+      #endregion
+
+      private System.Windows.Forms.Button SendaBtn;
+      private System.Windows.Forms.ListBox LogLB;
+      private System.Windows.Forms.Button SendbBtn;
+      private System.Windows.Forms.Button SendcBtn;
+      private System.Windows.Forms.Button SenddBtn;
+      private System.Windows.Forms.Timer timer1;
+   }
+}
+
---
+++ WifiSocketTest/WifiSocketTest/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using Microsoft.VisualBasic;
+
+namespace WifiSocketTEst
+{
+   public partial class Form1 : Form
+   {
+      IPAddress ipAddress;
+      IPEndPoint remoteEP;
+      Socket client;
+      Button[] AntButtons;
+      public Form1()
+      {
+         InitializeComponent();
+         ipAddress = IPAddress.Parse("192.168.0.152");
+         remoteEP = new IPEndPoint(ipAddress, 8000);
+         AntButtons = new Button[4];
+         AntButtons[0] = SendaBtn;
+         AntButtons[1] = SendbBtn;
+         AntButtons[2] = SendcBtn;
+         AntButtons[3] = SenddBtn;
+         SendaBtn.Text = Properties.Settings.Default.Antenna1;
+         SendbBtn.Text = Properties.Settings.Default.Antenna2;
+         SendcBtn.Text = Properties.Settings.Default.Antenna3;
+         SenddBtn.Text = Properties.Settings.Default.Antenna4;
+      }
+
+      public int SendGetResponse(out string response, string message)
+      {
+         int rv = 0;
+         response = null;
+         client = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+         try
+         {
+            byte[] buffer = Encoding.ASCII.GetBytes(message);
+            client.Connect(remoteEP);
+            int bytessent = client.Send(buffer);
+
+            byte[] receivebuff = new byte[256];
+            client.Receive(receivebuff, 256, SocketFlags.None);
+            response = Encoding.ASCII.GetString(receivebuff);
+            client.Close();
+         }
+         catch (Exception ex)
+         {
+            rv = -1;
+         }
+
+         return rv;
+      }
+      public int writelog(string message)
+      {
+         LogLB.Items.Add(message);
+         LogLB.SelectedIndex = LogLB.Items.Count - 1;
+         return 0;
+      }
+
+      private void timer1_Tick(object sender, EventArgs e)
+      {
+         string response;
+         SendGetResponse(out response, "s");
+         int v = 0;
+         int.TryParse(response, out v);
+         for (int i = 0; i < 4; i++)
+         {
+            Color color = Color.Gray;
+            if (v == (1 << i))
+               color = Color.Green;
+            AntButtons[i].BackColor = color;
+         }
+         // writelog("Status " + response);
+      }
+
+
+
+      private void SendaBtn_MouseUp(object sender, MouseEventArgs e)
+      {
+         if (e.Button == MouseButtons.Right)
+         {
+            string str = Interaction.InputBox("Enter new button label", SendaBtn.Text);
+            Properties.Settings.Default.Antenna1 = str;
+            SendaBtn.Text = str;
+            Properties.Settings.Default.Save();
+         }
+         else if (e.Button == MouseButtons.Left)
+         {
+            string response;
+            SendGetResponse(out response, "1");
+            writelog("Send 1: " + response);
+         }
+      }
+
+      private void SendbBtn_MouseUp(object sender, MouseEventArgs e)
+      {
+         if (e.Button == MouseButtons.Right)
+         {
+            string str = Interaction.InputBox("Enter new button label", SendbBtn.Text);
+            Properties.Settings.Default.Antenna2 = str;
+            SendbBtn.Text = str;
+            Properties.Settings.Default.Save();
+         }
+         else if (e.Button == MouseButtons.Left)
+         {
+            string response;
+            SendGetResponse(out response, "2");
+            writelog("Send 2: " + response);
+         }
+      }
+
+      private void SendcBtn_MouseUp(object sender, MouseEventArgs e)
+      {
+         if (e.Button == MouseButtons.Right)
+         {
+            string str = Interaction.InputBox("Enter new button label", SendcBtn.Text);
+            Properties.Settings.Default.Antenna3 = str;
+            SendcBtn.Text = str;
+            Properties.Settings.Default.Save();
+         }
+         else if (e.Button == MouseButtons.Left)
+         {
+            string response;
+            SendGetResponse(out response, "3");
+            writelog("Send 3: " + response);
+         }
+      }
+      private void SenddBtn_MouseUp(object sender, MouseEventArgs e)
+      {
+         if (e.Button == MouseButtons.Right)
+         {
+            string str = Interaction.InputBox("Enter new button label", SenddBtn.Text);
+            Properties.Settings.Default.Antenna4 = str;
+            SenddBtn.Text = str;
+            Properties.Settings.Default.Save();
+         }
+         else if (e.Button == MouseButtons.Left)
+         {
+            string response;
+            SendGetResponse(out response, "4");
+            writelog("Send 4: " + response);
+         }
+      }
+   }
+}
